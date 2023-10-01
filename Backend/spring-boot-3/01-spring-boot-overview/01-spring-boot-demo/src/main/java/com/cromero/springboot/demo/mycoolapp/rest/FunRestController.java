995e1be7819fb299b7eb284ec5a431a9318fe6da
Carlos Romero <carlosisairomero@gmail.com>
2023-09-30 19:18:05 -0600

    Spring Boot3 Quick Start

---
+++ Backend/spring-boot-3/01-spring-boot-overview/01-spring-boot-demo/src/main/java/com/cromero/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -0,0 +1,16 @@
+package com.cromero.springboot.demo.mycoolapp.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestController {
+
+    // expose "/" that return "Hellow World"
+
+    @GetMapping("/")
+    public String sayHello() {
+        return "Hello World!";
+    }
+
+}
